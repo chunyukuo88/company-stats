c20109389b1b037ae24438bf33f89f4e4fee34d7
chunyukuo88 <alexgochenour@gmail.com>
2020-01-20 18:57:47 -0500

    Now the company info is displayed on the page. I will come back to this later, but for now it is time to make the function that will render an input field and some layout elements.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 const fetcher = require('../src/js/fetch');
 
-let company = fetcher.fetchCompanyInfo('AAPL');
-console.log(company);
+let enterprise;
+
+fetcher.fetchCompanyInfo('GOOG')  
+    .then(company => {
+            console.log("PRICE:" + company.price); 
+
+            let app = document.getElementById('app');  
+            let element = document.createElement('p');  
+
+            element.innerText = company.companyName;  
+            app.append(element);
+            }
+        );
--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,25 +1,15 @@
 module.exports = {
 
-    async fetchCompanyInfo(stockSymbol){
-
-        const url = "https://financialmodelingprep.com/api/v3/company/profile/" 
-                    + stockSymbol;
-        const options = { method: "GET", 
-                          headers: { "Content-type": "application/JSON" } 
-                        };
-        
-        let companyStats = {};
-        
-        await fetch(url, options)
+    fetchCompanyInfo(stockSymbol){
+        const url = "https://financialmodelingprep.com/api/v3/company/profile/" + stockSymbol;
+        const options = { method: "GET", headers: { "Content-type": "application/JSON" }};     
+     
+        return fetch(url, options)
                         .then(res => res.json())
-                        .then(data => {
-                            console.log("The goods:\n\n" + data.profile.price);
-                            companyStats = data.profile;
-                        })
+                        .then(data => data.profile)
                         .catch("Fetch failed");
-
-
-        return companyStats;
     }
+
+
 
 }
