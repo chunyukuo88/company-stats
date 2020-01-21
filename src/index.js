04333c255bdb8a473386e0e9cb160a845fa55099
chunyukuo88 <alexgochenour@gmail.com>
2020-01-20 19:39:04 -0500

    Began styling for placement of elements.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 const fetcher = require('../src/js/fetch');
 
-let enterprise;
+
+let app = document.getElementById('app');
+let wrapper = document.createElement('wrapper');
+wrapper.classList.add("wrapper");
+
+let leftSection = document.createElement('section');
+leftSection.id = 'left-section';
+let rightSection = document.createElement('section');
+rightSection.id = 'right-section';
+
+wrapper.appendChild(leftSection);
+wrapper.appendChild(rightSection);
+app.append(wrapper);
 
 fetcher.fetchCompanyInfo('GOOG')  
     .then(company => {
-            console.log("PRICE:" + company.price); 
 
-            let app = document.getElementById('app');  
-            let element = document.createElement('p');  
-
+            let name = document.createElement('h1');  
             element.innerText = company.companyName;  
-            app.append(element);
+            leftSection.append(element);
+
+            let ceo = document.createElement('h1');  
+            element.innerText = company.ceo;  
+            righttSection.append(element);
             }
         );
